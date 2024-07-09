b82845417b7c6326634b93660ebdba6a3c98bb19
urimanul <myopera9@gmail.com>
2024-07-10 04:48:17 +0900

    Update sidebar.py

--- knowledge_gpt/components/sidebar.py
+++ knowledge_gpt/components/sidebar.py
@@ -21,32 +21,82 @@ def sidebar():
 
         flgCheck_A = st.checkbox('登録キー参照')
         if flgCheck_A:
+            # Input User ID
+            SPOID = st.text_input(
+                "SPOID",
+                type="password",
+                disabled=False,
+                placeholder="SPOIDを入力",
+                help="SPOID",  # noqa: E501
+                ),
+            )
+
+            USER = st.text_input(
+                "USERID",
+                type="password",
+                disabled=False,
+                placeholder="USERIDを入力",
+                help="USERID",  # noqa: E501
+                ),
+            )
+
+            PASSKEY = st.text_input(
+                "PASSKEY",
+                type="password",
+                disabled=False,
+                placeholder="PASSKEYを入力",
+                help="PASSKEY",  # noqa: E501
+                ),
+            )
+
+            HOST = st.text_input(
+                "HOST",
+                type="text",
+                disabled=False,
+                placeholder="HOSTを入力",
+                help="HOST",  # noqa: E501
+                ),
+            )
+
+            DBNAME = st.text_input(
+                "DBNAME",
+                type="text",
+                disabled=False,
+                placeholder="DBNAMEを入力",
+                help="DBNAME",  # noqa: E501
+                ),
+            )
+            
             # DBへ接続
             conn = mysql.connector.connect(
-            user='smairuser',
-            password='smairuser',
-            host='www.ryhintl.com',
-            database='smair',
+            user=USER,
+            password=PASSKEY,
+            host=HOST,
+            database=DBNAME,
             port=36000
             )
 
-            # DBの接続確認
-            if not conn.is_connected():
-                raise Exception("MySQLサーバへの接続に失敗しました")
+            flgButton = st.button('認証')
+            if flgButton:
+                # DBの接続確認
+                if not conn.is_connected():
+                    raise Exception("MySQLサーバへの接続に失敗しました")
 
-            cur = conn.cursor(dictionary=True)  # 取得結果を辞書型で扱う設定
+                cur = conn.cursor(dictionary=True)  # 取得結果を辞書型で扱う設定
 
-            query__for_fetching = """
-            SELECT api_key FROM openai_payload;
-            """
+                query__for_fetching = """
+                SELECT api_key FROM openai_payload;
+                """
 
-            cur.execute(query__for_fetching)
+                cur.execute(query__for_fetching)
 
-            for fetched_line in cur.fetchall():
-                #st.session_state["OPENAI_API_KEY"] = fetched_line['api_key']
-                api_key_input = fetched_line['api_key']
+                for fetched_line in cur.fetchall():
+                    #st.session_state["OPENAI_API_KEY"] = fetched_line['api_key']
+                    api_key_input = fetched_line['api_key']
 
-            cur.close()
+                cur.close()
+            else:
+                st.write("認証情報が正しくありません。")
         else:
             api_key_input = st.text_input(
                 "OpenAI API Key",
