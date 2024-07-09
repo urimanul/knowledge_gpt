12607384699b6d2f3763162854cdf581679ffb5d
urimanul <myopera9@gmail.com>
2024-07-10 04:04:19 +0900

    Update sidebar.py

--- knowledge_gpt/components/sidebar.py
+++ knowledge_gpt/components/sidebar.py
@@ -18,19 +18,44 @@ def sidebar():
             "3. ドキュメントについて質問してください💬\n"
         )
 
-        flgCheck_A = st.checkbox('登録キーから取得')
+        flgCheck_A = st.checkbox('キー入力')
         if flgCheck_A:
-            st.text('Checkbox A has checked')
-        
-        api_key_input = st.text_input(
-            "OpenAI API Key",
-            type="password",
-            disabled=True,
-            placeholder="ここに OpenAI API キーを貼り付けます (sk-...)",
-            help="APIキーはhttps://platform.openai.com/account/api-keys から取得できます。",  # noqa: E501
-            value=os.environ.get("OPENAI_API_KEY", None)
-            or st.session_state.get("OPENAI_API_KEY", ""),
-        )
+            #st.text('Checkbox A has checked')
+            api_key_input = st.text_input(
+                "OpenAI API Key",
+                type="password",
+                disabled=False,
+                placeholder="ここに OpenAI API キーを貼り付けます (sk-...)",
+                help="APIキーはhttps://platform.openai.com/account/api-keys から取得できます。",  # noqa: E501
+                value=os.environ.get("OPENAI_API_KEY", None)
+                or st.session_state.get("OPENAI_API_KEY", ""),
+            )
+        else:
+            # DBへ接続
+            conn = mysql.connector.connect(
+            user='smairuser',
+            password='smairuser',
+            host='www.ryhintl.com',
+            database='smair',
+            port=36000
+            )
+
+            # DBの接続確認
+            if not conn.is_connected():
+                raise Exception("MySQLサーバへの接続に失敗しました")
+
+            cur = conn.cursor(dictionary=True)  # 取得結果を辞書型で扱う設定
+
+            query__for_fetching = """
+            SELECT api_key FROM openai_payload;
+            """
+
+            cur.execute(query__for_fetching)
+
+            for fetched_line in cur.fetchall():
+                st.session_state["OPENAI_API_KEY"] = fetched_line['api_key']
+
+        cur.close()
 
         st.session_state["OPENAI_API_KEY"] = api_key_input
 
