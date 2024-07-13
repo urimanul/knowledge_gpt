11578c2506a60b3e8bde94a55a2ef3cf594d9906
urimanul <myopera9@gmail.com>
2024-07-13 20:12:58 +0900

    Update main.py

--- knowledge_gpt/main.py
+++ knowledge_gpt/main.py
@@ -47,7 +47,7 @@ if not openai_api_key:
 uploaded_file = st.file_uploader(
     "PDF、docx、または txt ファイルをアップロードする",
     type=["pdf", "docx", "txt"],
-    accept_multiple_files=True,
+    #accept_multiple_files=True,
     help="スキャンされたドキュメントはまだサポートされていません。",
 )
 
